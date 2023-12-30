ae3ec334fd4b5c41182204e5d01ea14e4fc89171
Pari Gupta <143111684+PARIGUPTA0011@users.noreply.github.com>
2023-12-30 20:47:29 +0530

    Update A2_Q4.cpp

--- A2_Q4.cpp
+++ A2_Q4.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc, char **argv){
+    int n;
+    cin >> n;
+
+    //write your code here
+    for(int i = 1 ; i<= n ; i++){
+    for(int j = 1; j <=i-1; j++){
+        cout<<" ";
+    }
+    for(int j = n-i+1 ; j>=1 ; j-- ){
+        cout<<"*";
+    }
+cout<<endl;
+}
+
+}
