3020b504c32501b3f5d4b44340f915637755d232
Pari Gupta <143111684+PARIGUPTA0011@users.noreply.github.com>
2024-01-03 00:50:27 +0530

    Update A2_Q9.cpp

--- A2_Q9.cpp
+++ A2_Q9.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc, char **argv){
+    int n;
+    cin >> n;
+
+    //write your code here
+    for(int i = 1 ; i<= (n+1)/2 ; i++){
+    // star
+    for(int j = 1 ; j<=n ; j++ ){
+        if(i==j || i+j == n+1){
+            cout<<" * ";
+        } else {
+            cout<<"  ";
+        }
+    }
+    //space
+    for(int j =1 ; j<=n-2*i; j++){
+        cout<<"  ";
+    } 
+cout<<endl;  
+}
+for(int i = (n-1)/ 2 ; i>=1 ; i--){
+    for(int j = 1 ; j <= n ; j++){
+        if(i+j == n+1 || i==j){
+            cout<<" * ";
+        } else {
+            cout<<"  ";
+        }
+    
+    }
+    for(int j = 1 ; j<= 2*i-1 ; j++){
+        cout<<"  ";
+    }
+cout<<endl;
+}
+
+}
