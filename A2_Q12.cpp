aaea0aea4445f7a8e639333e78fb742feb926490
Pari Gupta <143111684+PARIGUPTA0011@users.noreply.github.com>
2024-01-03 14:46:27 +0530

    Update A2_Q12.cpp

--- A2_Q12.cpp
+++ A2_Q12.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc, char **argv){
+    int n;
+    cin >> n;
+    
+    //write your code here
+        int a = 0, b = 1;
+
+    for (int i = 1; i <= n; i++) {
+        int c = a;
+        for (int j = 1; j <= i; j++) {
+            cout << c << "\t";
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        cout << endl;
+    }
+    
+}
