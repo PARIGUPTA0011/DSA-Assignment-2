622741aa4656b3083f24b7eef3b3283925818098
Pari Gupta <143111684+PARIGUPTA0011@users.noreply.github.com>
2024-01-03 13:55:18 +0530

    Update A2_Q10.cpp

--- A2_Q10.cpp
+++ A2_Q10.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc, char **argv){
+    int n;
+    cin >> n;
+
+    //write your code here
+    for(int i = 1 ; i<=(n+1)/2 ; i++){
+    for(int j = 1 ; j<= n ; j++){
+        if(i+j==(n+3)/2 ||j-i == (n-1)/2 ){
+            cout<<"*";
+        } else {
+            cout<<" ";
+        }
+    }
+cout<<endl;
+}
+
+for(int i = (n-1)/2 ; i >= 1 ; i--){
+    for(int j = 1; j<= n ; j++){
+          if(i+j == (n+3)/2 || j-i == (n-1)/2){
+               cout<<"*";
+
+          } else {
+            cout<<" ";
+          }
+}
+cout<<endl;
+}
+    
+}
