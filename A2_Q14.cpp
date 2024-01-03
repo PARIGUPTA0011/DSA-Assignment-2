1878404e083c9f0d4a8b67adacb6c008d6410287
Pari Gupta <143111684+PARIGUPTA0011@users.noreply.github.com>
2024-01-03 14:59:26 +0530

    Update A2_Q14.cpp

--- A2_Q14.cpp
+++ A2_Q14.cpp
@@ -1,1 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(int agrc, char** argv){
+    int n;
+    cin >> n;
+    
+    //write your code here
+    for(int i = 1 ; i<= 10 ; i++){
+    cout<<n<<"*"<<i<<"="<<n*i;
+    cout<<endl;
+}
 
+    
+}
