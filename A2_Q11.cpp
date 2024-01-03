599213b86f7e80a24413f8527d397d794432ab75
Pari Gupta <143111684+PARIGUPTA0011@users.noreply.github.com>
2024-01-03 14:07:26 +0530

    Update A2_Q11.cpp

--- A2_Q11.cpp
+++ A2_Q11.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc, char **argv){
+    int n;
+    cin >> n;
+    
+    //write your code here
+    int count = 1;
+for(int i = 1 ; i<= n ; i++){
+    for(int j = 1; j<=i; j++){
+        cout<<count<<" ";
+        count++;
+    }
+cout<<endl;
+
+}
+
+    
+}
