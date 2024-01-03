b85323d34fa6b40af998648b0a719364bd071e06
Pari Gupta <143111684+PARIGUPTA0011@users.noreply.github.com>
2024-01-03 16:25:21 +0530

    Update A2_Q16.cpp

--- A2_Q16.cpp
+++ A2_Q16.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char**argv){
+    int n;
+    cin >> n;
 
+    //write your code here
+    for(int i = 1 ; i<= n ; i++){
+    //num
+    for(int j = 1 ; j<= i; j++){
+        cout<<j;
+    }
+    //space
+    for(int j = 1 ; j<= 2*n - 2*i -1 ; j++){
+        cout<<" ";
+    }
+    for(int j = i; j>=1 ; j--){
+        if(j==n){
+            continue;
+        }
+         cout<<j;
+    }
+
+cout<<endl;
+
+}
+    
+}
